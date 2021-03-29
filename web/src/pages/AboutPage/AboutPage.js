909a37a957c0a1910463e67d9c91d308e753bca0
maklad42 <maklad42+git@gmail.com>
2021-03-29 22:05:40 +0900

    Add About page, and BlogLayout wrapper component. 210329

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+  return (
+    <>
+      <p>
+        This site was create to dazzle the world with my mastery of Redwood JS:
+        <br />
+        Gaze upon my mighty works and despair, ye mere mortals!
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -3,19 +3,9 @@ import { Link, routes } from '@redwoodjs/router'
 const HomePage = () => {
   return (
     <>
-      <h1>HomePage</h1>
-      <p>
-        Ooh! Look at me. My first RedWood.js page all nicely automatically
-        gerenated with routes and tests and everything a React component could
-        want.
-      </p>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <main>
+        <p>Some pretty cool content coming soon!</p>
+      </main>
     </>
   )
 }
