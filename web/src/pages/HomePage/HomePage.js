50f22d165a8503fc805b3d5a1e833b723fbbd8bc
maklad42 <maklad42+git@gmail.com>
2021-03-31 23:53:50 +0900

    Add a home page showing a list of blog posts using a cell. 210331

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,13 +1,7 @@
 import { Link, routes } from '@redwoodjs/router'
-
+import BlogPostsCell from 'src/components/BlogPostsCell'
 const HomePage = () => {
-  return (
-    <>
-      <main>
-        <p>Some pretty cool content coming soon!</p>
-      </main>
-    </>
-  )
+  return <BlogPostsCell />
 }
 
 export default HomePage
